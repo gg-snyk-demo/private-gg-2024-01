4a9ef4e1af42d091a49431e2f8477bdec5667994
Bruce Banner <bruce-banner-snyk@protonmail.com>
2024-01-28 04:03:31 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 import antigravity
 
-CERT="""-----BEGIN DSA PRIVATE KEY-----
-MIIBvAIBAAKBgQDSZBYSJBFGnwZZLNAKROahhI5sOlb8Au36W/mI6b74Iaou5vAw
-LV9LBVYf32TphQ5gHHwP9X5yz6H/vVjrzTH5XgNn3OpClnUlRj0guFnupZHdZ40w
-Euh0019fox/bXQGAili2fIwtHJ4yUIdWugafeivno9o7c9EAm9YYFIBtowIVAKHn
-3bvf5U+iFtpq47KU8WLn1wotAoGAF+nDckqAM5V/zPwDksMldADxpsPEd3zmh+Pi
-dWPfVTzYYo5jiwQFu4Yi5emmDCMgcfjicC2iuA4bUmd8IjF3Dl37ijyy2MlDOj+U
-jj0zVhSKQtXjWl7g6qVKx/Hx/W3JiuzWshxbBQLE+9xk8nhfS10vzOofls+2+F7z
-sfdFyIkCgYEAhHZpPbmlXF2ZXKp2lya1pkWHHmPM058MmXh7p5wkx9NIiYUp4yeQ
-tgdCXpcjt+ofCppmjN+wTdo0rOjeECVtl8itdXydopqaQfOB+7bkoU8puZVS9eRG
-oxE4Zzrta/GhAnlipZslHYDE0kmWGuww3bL94iaaY2vvtHio5ZjmWm0CFQCGlykh
-tRES1JmwJyKQw/Cgq++9qw==
------END DSA PRIVATE KEY-----
-"""
+SEED=772d4c6d-80d7-45fd-9287-c351693f55b4
 
 def main():
     antigravity.fly()
